6f591831440f7875d5633dd361df97c14c318e06
Ksenia Pilshchikova <ksushapi@gmail.com>
2024-02-17 13:47:31 +0100

    feat: login and create player

--- index.ts
+++ index.ts
@@ -1,33 +1,35 @@
 import WebSocket, { WebSocketServer } from 'ws';
 import { httpServer } from "./src/http_server/index.ts";
+import { EReqType, ILoginReq, ILoginRes } from "./src/models/gameModels.ts";
+import { loginAndCreatePlayer } from './src/controllers/player/loginAndCreatePlayer.ts';
 
 const HTTP_PORT = 8181;
+const WS_PORT = 3000;
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 
 httpServer.listen(HTTP_PORT);
 
-const wsServer = new WebSocketServer({ port: 3000 });
+const wsServer = new WebSocketServer({ port: WS_PORT }, () => {
+    console.log(`Start new WebSocket on ws://localhost:${WS_PORT}!`);
+});
 
-wsServer.on('connection', function (socket) {
-    console.log(`New WebSocket connection established`);
-    
-    socket.on('message', function (message) {
-        console.log('Received message:', message.toString());
+wsServer.on('connection', (socket: WebSocket) => {
+    socket.on('message', async (message) => {
+        const req: ILoginReq = JSON.parse(message.toString());
+        let res: ILoginRes | undefined;
+        
+        if (req.type === EReqType.REG) {
+            res = loginAndCreatePlayer(req);
+        }
+        socket.send(JSON.stringify(res));
     });
-
+    
     socket.on('close', function () {
         console.log('WebSocket connection closed');
     });
 });
 
-const host = `ws://localhost:3000`;
-const socket = new WebSocket(host);
-
-socket.on('open', function () {
-    console.log(`New WebSocket connection on ${host} established`);
-    socket.send('Hello server!');
-});
 const handleTerminationSignals = () => {
     console.log("Terminating program...");
     wsServer.close(() => {
